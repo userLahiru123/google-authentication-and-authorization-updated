5e25e11ebd5977c41d28063c915118b23f08369f
userLahiru123 <thiwanka631@gmail.com>
2024-09-09 12:16:05 +0530

    remove WHERE clause from getUsers function

--- api/users/user.service.js
+++ api/users/user.service.js
@@ -4,8 +4,7 @@ module.exports = {
   getUsers: async (user_sub) => {
     await pool.query(`SELECT set_config('app.sub','${user_sub}',true)`);
     const result = await pool.query(
-      `SELECT name, email FROM "user" WHERE sub = $1`,
-      [user_sub]
+      `SELECT name, email FROM user`
     );
     
     return result;
