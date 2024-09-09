b25cbd1d64135baa6c6635feab58d42ea96ca673
userLahiru123 <thiwanka631@gmail.com>
2024-09-09 11:40:47 +0530

    row level security

--- api/users/user.service.js
+++ api/users/user.service.js
@@ -2,6 +2,7 @@ const pool = require("../../config/database");
 
 module.exports = {
   getUsers: async (user_sub) => {
+    await pool.query(`SELECT set_config('app.sub','${user_sub}',true)`);
     const result = await pool.query(
       `SELECT name, email FROM "user" WHERE sub = $1`,
       [user_sub]
